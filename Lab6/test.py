12a644fc96b6581bb913fd4f7ab0b27d4aad2d04
Georgi Galchev <georgigalchev03@schoolmath.eu>
2022-11-24 15:19:38 +0200

    a

--- Lab6/test.py
+++ Lab6/test.py
@@ -1,1 +1,9 @@
-print("a")
+class Student():
+    def __init__(self, name, fac_num, svg_grade):
+        self.name = name
+        self.fac_num = fac_num
+        self.svg_grade = svg_grade
+
+
+ge6 = Student("Gosho", "121222030",5.5)
+print(ge6.fac_num)
